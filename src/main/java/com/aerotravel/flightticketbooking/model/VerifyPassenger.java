4b2651b82c85be62d06fa0707ca3a871353b9d7e
dpqls <dpqls0625@naver.com>
2022-08-04 20:48:42 +0900

    로그인후 예약조회

---
+++ src/main/java/com/aerotravel/flightticketbooking/model/VerifyPassenger.java
@@ -0,0 +1,57 @@
+package com.aerotravel.flightticketbooking.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "verify_passenger")
+public class VerifyPassenger {
+    @Id
+    @GeneratedValue
+    private long id;
+    @ManyToOne
+    private Flight flight;
+    @ManyToOne
+    private Passenger passenger;
+    @ManyToOne
+    private User user;
+
+    public VerifyPassenger(){
+
+    }
+    public VerifyPassenger(Flight flight, Passenger passenger, User user){
+        this.flight=flight;
+        this.passenger=passenger;
+        this.user=user;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void getId(long id) {
+        this.id = id;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
